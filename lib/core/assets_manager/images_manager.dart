b3891d50c32437ebf1b972ac0b840d4079efc45e
Mijo-sh <majedaalshalabi207@gmail.com>
2026-05-13 01:06:50 +0300

    add folders

---
+++ lib/core/assets_manager/images_manager.dart
@@ -0,0 +1,3 @@
+class ImagesManager {
+  static const String imagesPath = 'assets/images/';
+}
---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,20 @@
+import 'route_name.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  static final GoRouter appRouter = GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: RouteName.splash,
+        builder: (context, state) => Placeholder()
+      ),
+
+      GoRoute(
+          path: RouteName.onboarding,
+          builder: (context, state) => Placeholder()
+      ),
+    ]
+  );
+}
