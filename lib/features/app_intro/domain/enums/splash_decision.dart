5d087ce53a23798e6986c93aa4cec4e42b53d970
Mijo-sh <majedaalshalabi207@gmail.com>
2026-06-02 22:16:53 +0300

    edit app name and icon

---
+++ lib/features/app_intro/domain/enums/splash_decision.dart
@@ -0,0 +1,6 @@
+enum SplashDecision {
+  onboarding,
+  logIn,
+  studentHomeShell,
+  parentHome
+}
---
+++ lib/features/app_intro/presentation/bloc/splash/splash_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:bloc/bloc.dart';
+
+part 'splash_event.dart';
+part 'splash_state.dart';
+
+class SplashBloc extends Bloc<SplashEvent, SplashState> {
+  SplashBloc() : super(SplashInitial()) {
+    on<SplashEvent>((event, emit) {
+    });
+  }
+}
